c2f3bbfc3ef77d67cb6b86dbae92b424be5c2246
Akiva1992 <breuerakiva@gmail.com>
2022-08-21 12:49:52 +0300

    phone version basic function working, still need to finish end game.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -25,12 +25,12 @@ userFemale.addEventListener("click", appendUser);
 // Implements User avatar on game screen.
 function appendUser(){
     if (this.classList.contains("female-user-avatar")){
-        userGameScreenAvatar.src = "./imgs/female.png"
+        userGameScreenAvatar.src = "./imgs/femaleOriginal.png"
         this.classList.add("selected");
         userMale.classList.remove("selected");
     }
     else{
-        userGameScreenAvatar.src = "./imgs/male.png"
+        userGameScreenAvatar.src = "./imgs/maleOriginal.png"
         this.classList.add("selected");
         userFemale.classList.remove("selected");
     }
@@ -43,11 +43,13 @@ function appendUser(){
 computerAvatarAj.addEventListener("click", appendComputer);
 computerAvatarLucy.addEventListener("click", appendComputer);
 computerAvatarLayla.addEventListener("click", appendComputer);
+const profilePic = document.querySelector(".profile-pic");
 
 
 function appendComputer(){
     if (this.classList.contains("aj")){
-        computerGameScreenAvatar.src = "./imgs/johnLeftOrginalSz.png"
+        computerGameScreenAvatar.src = "./imgs/johnLeftOriginal.png"
+        profilePic.src = "./imgs/johnOriginal.png";
         computerGameScreenAvatar.classList.add("aj")
         computerGameScreenAvatar.classList.remove("lucy")
         computerGameScreenAvatar.classList.remove("layla")
@@ -56,8 +58,9 @@ function appendComputer(){
         computerAvatarLayla.classList.remove("selected");
     }
     else if (this.classList.contains("lucy")){
-        computerGameScreenAvatar.src = "./imgs/lucyLeftOrginalSz.png"
-        computerGameScreenAvatar.classList.add("lucy-computer")
+        computerGameScreenAvatar.src = "./imgs/lucyLeftOriginal.png"
+        profilePic.src = "./imgs/lucyOriginal.png"
+        computerGameScreenAvatar.classList.add("lucy")
         computerGameScreenAvatar.classList.remove("aj")
         computerGameScreenAvatar.classList.remove("layla")
         this.classList.add("selected");
@@ -66,6 +69,7 @@ function appendComputer(){
     }
     else{
         computerGameScreenAvatar.src = "./imgs/laylaLeftOriginal.png"
+        profilePic.src = "./imgs/laylaOriginal.png"
         computerGameScreenAvatar.classList.add("layla")
         computerGameScreenAvatar.classList.remove("aj")
         computerGameScreenAvatar.classList.remove("lucy")
@@ -107,5 +111,190 @@ function startGameFunc(){
 
 
 
+//////////////////Game page///////////////////////////////////////////
+
+const newPostBtn = document.getElementById("new-post");
+const avatarReaction= document.querySelector(".avatar-reaction");
+const scenePara = document.querySelector(".scene-para");
+const sceneImg = document.querySelector(".scene-img");
+const sceneVideo = document.querySelector(".scene-video");
+const sceneContainer= document.querySelector(".scene-container");
+const buttonList = document.querySelectorAll(".selection-button");
+
+// Adds initial class name to enable new scene btn.
+avatarReaction.classList.add("result-showing");
 
 
+const paraArr=['"Time is precious. Waste it wisely."','"Back in 5 minutes (If not, read this status again)."','`"If you really loved me, you would say it on my Facebook Wall."','"Staying connected is more important than making your point."','"Staying connected is more important than making your point."','"Why do you think you and I are such good friends?"'];
+
+function randomPara() {
+    let randomPara = Math.floor(Math.random() * paraArr.length)
+    return randomPara
+}
+
+const videoArr =["./vids/ducks.mp4","./vids/dog.mp4","./vids/horse.mp4","./vids/skateboarding.mp4","./vids/kite.mp4"];
+
+
+function randomVideo() {
+    let randomVideo = Math.floor(Math.random() * videoArr.length)
+    return randomVideo
+}
+
+
+function randomIndexgenerator() {
+    let randomIndex = Math.floor(Math.random() * videoArr.length)
+    return randomIndex
+}
+
+// Click event listener for new scene btn.
+newPostBtn.addEventListener("click",implementScene);
+
+// Implements the scene in to the scene div.
+function implementScene(){
+    if (avatarReaction.classList.contains("result-showing")){
+        let newScene = randomIndexgenerator();
+        if (newScene == 0){
+            let i = randomPara();
+            scenePara.classList.add("show");
+            sceneImg.classList.remove("show");
+            sceneVideo.classList.remove("show");
+            scenePara.textContent = paraArr[i];
+            sceneContainer.classList="scene-of-text";
+        }
+        else if (newScene == 1){
+            scenePara.classList.remove("show");
+            sceneImg.classList.add("show");
+            sceneVideo.classList.remove("show");
+            sceneImg.src="https://picsum.photos/300/150";
+            sceneContainer.classList="scene-of-image";
+        }
+        else{
+            let j = randomVideo();
+            scenePara.classList.remove("show");
+            sceneImg.classList.remove("show");
+            sceneVideo.classList.add("show");
+            sceneVideo.src = videoArr[j];
+            sceneContainer.classList="scene-of-video";
+        }
+        // // These two lines remove the last result from the div, and add class to enable user to react 
+        // resultDiv.textContent="";
+        avatarReaction.classList.remove("result-showing");
+    }
+}
+
+
+
+// btn event listener.
+buttonList.forEach(btn => btn.addEventListener("click", playRound))
+
+const charactersObject = {
+    aj: ["scissors","rock","paper"],
+    lucy:["scissor","paper","rock"],
+    layla:["rock","scissors","paper"]
+}
+
+
+// btn event listener.
+buttonList.forEach(btn => btn.addEventListener("click", playRound))
+
+
+let playerScore = 0;
+
+// Is activated when user presses one of his reaction btns.
+// This function determines who won the round.
+function playRound(){
+    if (!avatarReaction.classList.contains("result-showing")){
+    
+        // Gets the random index generated in randomIndexGenerator
+        let sceneIndexValue ="";
+        if (sceneContainer.classList.contains("scene-of-text")){
+            sceneIndexValue = 0;
+        }
+        else if (sceneContainer.classList.contains("scene-of-image")){
+            sceneIndexValue = 1;
+        }
+        else if (sceneContainer.classList.contains("scene-of-video")){
+            sceneIndexValue = 2;
+        }
+
+        console.log(sceneIndexValue)
+        
+        
+        ////text==index 0, image==index 1, video==index 2.
+    // Gets the value of the computer array at location of the random index generated.
+        let computerSelection ="";
+        if (computerGameScreenAvatar.classList.contains("aj")){
+            computerSelection = charactersObject.aj[sceneIndexValue];
+        }
+        else if (computerGameScreenAvatar.classList.contains("lucy")){
+            computerSelection = charactersObject.lucy[sceneIndexValue];
+        }
+        else if (computerGameScreenAvatar.classList.contains("layla")) {
+            computerSelection = charactersObject.layla[sceneIndexValue];
+        }
+        console.log(computerSelection)    
+
+        
+        ////like==rock, smile==paper, heart==scissors.
+
+        if (computerSelection == "rock"){
+            if (this.classList.contains("like")){
+                avatarReaction.textContent = "Okay..";
+                avatarReaction.classList.add("result-showing");
+                // runningScore.textContent= `${p + playerScore} ${c + computerScore}`
+            }
+            else if (this.classList.contains("smile")){
+                playerScore=0;
+                avatarReaction.textContent = "Hmm..";
+                avatarReaction.classList.add("result-showing");
+                // runningScore.textContent= `${p + playerScore} ${c + computerScore}`
+            }
+            else{
+                playerScore++
+                avatarReaction.textContent = "Yess!";
+                avatarReaction.classList.add("result-showing");
+                // runningScore.textContent= `${p + playerScore} ${c + computerScore}`
+            }
+        }
+        else if (computerSelection == "paper"){
+            if (this.classList.contains("like")){
+                playerScore++
+                avatarReaction.textContent = "Yess!";
+                avatarReaction.classList.add("result-showing");
+                // runningScore.textContent= `${p + playerScore} ${c + computerScore}`
+            }
+            else if (this.classList.contains("smile")){
+                avatarReaction.textContent = "Okay..";
+                avatarReaction.classList.add("result-showing");
+                // runningScore.textContent= `${p + playerScore} ${c + computerScore}`
+            }
+            else{
+                playerScore=0;
+                avatarReaction.textContent = "Hmm..";
+                avatarReaction.classList.add("result-showing");
+                // runningScore.textContent= `${p + playerScore} ${c + computerScore}`
+
+            }
+        }
+        else {
+            if (this.classList.contains("like")){
+                playerScore=0;
+                avatarReaction.textContent = "Hmm.."
+                avatarReaction.classList.add("result-showing");
+                // runningScore.textContent= `${p + playerScore} ${c + computerScore}`
+            }
+            else if (this.classList.contains("smile")){
+                playerScore++
+                avatarReaction.textContent = "Yess!";
+                avatarReaction.classList.add("result-showing");
+                // runningScore.textContent= `${p + playerScore} ${c + computerScore}`
+
+            }
+            else{
+                avatarReaction.textContent = "Okay"
+                avatarReaction.classList.add("result-showing");
+                // runningScore.textContent= `${p + playerScore} ${c + computerScore}`
+            }
+        }
+    }
+}
